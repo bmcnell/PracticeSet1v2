1498075d1fe69fb531e5ef310894220a261123d2
Bonnie <Bonnie@128.4.98.119>
2016-09-15 16:52:00 -0400

    added test note for new repo

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -3,7 +3,8 @@ package pkgMain;
 public class cMain {
 	
 	public static void main(String[] args) {
-//test note
+//test note for new repo
+		
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
 	}
